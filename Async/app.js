a4130a45d528f8efe57e852913b3fc73cc3bd7c7
Abhishek <abhikam152002@gmail.com>
2026-01-08 15:30:49 +0530

    await

--- Async/app.js
+++ Async/app.js
@@ -11,3 +11,26 @@ greet()
     .catch((err) => {
         console.info(" Promise Showed Errr ", err);
     })
+
+
+// Async and Await basic :- 
+
+function getNum() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let num = Math.floor(Math.random() * 10) + 1;
+            console.log(num);
+            resolve();
+        }, 1000);
+    });
+};
+
+async function demo() {
+    let num1 = await getNum();
+    let num2 = await getNum();
+    let num3 = await getNum();
+    let num4 = await getNum();
+    console.log(num1, num2, num3,);
+}
+
+demo();
