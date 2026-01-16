1ffbb19a8bd8a09d8974b21d3390bedf966050c7
Abhishek <abhikam152002@gmail.com>
2026-01-16 20:05:01 +0530

    access json

---
+++ JSON_Learn/app.js
@@ -0,0 +1,6 @@
+// Understanding Two methods to access the JSON Data:- 
+let jsonResponse =
+    '{"fact":"A cat can sprint at about thirty-one miles per hour.","length":52}';
+
+// let validResponse = JSON.parse(jsonResponse);
+// console.info(validResponse);
