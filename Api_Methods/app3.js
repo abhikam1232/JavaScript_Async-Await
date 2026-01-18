9634242307d2470133fea644ae02078a56249358
Abhishek <abhikam152002@gmail.com>
2026-01-18 15:19:41 +0530

    fetch promise chaining

---
+++ Api_Methods/app3.js
@@ -0,0 +1,13 @@
+let myurl = "https://api.agify.io/?name=abhishek";
+
+fetch(myurl)
+    .then((result) => {
+        console.log(result);
+        return result.json();
+    })
+    .then((info) => {
+        console.log(info);
+    })
+    .catch((err) => {
+        console.log(err.count);
+    })
